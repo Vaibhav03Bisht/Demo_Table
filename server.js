bd559b0f7430614ee3d6d4a19e2048917a35f74e
Vaibhav03Bisht <vaibhav.bisht@appfire.com>
2025-12-01 19:50:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,4 +18,8 @@ app.get("/data.json", (req, res) => {
 });
 
 // Render requires listening on process.env.PORT
-const PORT = pr
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
